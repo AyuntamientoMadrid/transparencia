a93f650f035289493d3e8d0b1f1c40a76fbaf474
Juanjo Bazán <jjbazan@gmail.com>
2015-10-23 21:34:07 +0200

    adds i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_locale
+
+  private
+
+    def set_locale
+      if params[:locale] && I18n.available_locales.include?(params[:locale].to_sym)
+        session[:locale] = params[:locale]
+      end
+
+      session[:locale] ||= I18n.default_locale
+      I18n.locale = session[:locale]
+    end
 end
