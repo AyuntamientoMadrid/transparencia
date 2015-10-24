f49a351ce91b0e44dc3a554e8d99bc3736ba4ce9
rgarcia <voodoorai2000@gmail.com>
2015-10-24 18:03:18 +0200

    adds design placeholders

---
+++ app/controllers/designs_controller.rb
@@ -0,0 +1,12 @@
+class DesignsController < ApplicationController
+
+  def enquiry_index
+  end
+
+  def enquiry_form
+  end
+
+  def enquiry_show
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
+
+  get "designs/enquiry_index", to: "designs#enquiry_index"
+  get "designs/enquiry_form", to: "designs#enquiry_form"
+  get "designs/enquiry_show", to: "designs#enquiry_show"
 end
