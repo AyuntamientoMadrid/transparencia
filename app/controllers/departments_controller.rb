026234d733796a6b30115ca57c1babfb8b4bc856
Senén Rodero <senenrodero@gmail.com>
2015-10-24 17:43:53 +0200

    Adapt application to new desing. Fix broken specs.

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -2,6 +2,8 @@ class DepartmentsController < ApplicationController
   
   def index
     @areas = Area.all
+    @area = Area.find(params[:area_id])
+    @departments = Department.where(area_id: params[:area_id])
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#home'
-  resources :areas, only: [:index]
+  resources :areas, only: [:index] do
+    resources :departments, only: [:index]  
+  end
   resources :departments, only: [:show]
   resources :objectives, only: [:show, :edit, :update] 
 end
