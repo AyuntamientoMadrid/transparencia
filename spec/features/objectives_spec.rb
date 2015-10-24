d96aecc27ebf9ef1d58ebe86d710840a50dccef4
Sebastia <sebastia.roig@gmail.com>
2015-10-24 16:37:50 +0200

    Add objective update test

--- spec/features/objectives_spec.rb
+++ spec/features/objectives_spec.rb
@@ -28,6 +28,26 @@ feature 'Objectives' do
     within ('.edit_objective') do
       expect(page).to have_selector("#objective_title", objective.title )
       expect(page).to have_selector("#objective_description", objective.description )
+      expect(page).to have_selector("#objective_accomplished", "Cumplido" )
+      expect(find("#objective_accomplished")).to be_checked
     end
   end 
+
+  scenario 'Update objective' do  
+    visit edit_objective_path(objective)      
+    
+    expect(find("#objective_accomplished")).to be_checked
+
+    fill_in 'objective_title', with: 'edit titulo'
+    fill_in 'objective_description', with: 'edit descripcion'
+    uncheck 'Cumplido'
+    click_on('Actualizar')
+
+    expect(page).to have_content "edit titulo"
+    expect(page).to have_content "edit descripcion"
+    expect(find("#objective_accomplished")).not_to be_checked
+
+    expect(page).to have_content "El objetivo se ha actualizado correctamente."
+  end   
+  
 end
