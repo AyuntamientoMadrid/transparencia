d78a1c716ca79d3893d962397376258c6b18db0d
Senén Rodero <senenrodero@gmail.com>
2015-10-25 19:15:19 +0100

    Add has many relation to Page

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,6 @@
 class Page < ActiveRecord::Base
   belongs_to :parent, class_name: "Page", foreign_key: "parent_id"
+  has_many :pages, class_name: "Page", foreign_key: "parent_id"
 
   def level
     self.parent.present? ? self.parent.level + 1 : 1
