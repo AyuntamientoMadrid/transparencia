6e6658cbabbe683e49ce8c199de4d9b33c2e3aec
Carlos Guzmán <carlos@guzmanweb.com>
2015-10-25 18:22:12 +0100

    Pages index

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,10 @@
 class Page < ActiveRecord::Base
   belongs_to :parent, class_name: "Page", foreign_key: "parent_id"
+
+  def level
+    return 1 if self.parent.blank?
+    return 2 if self.parent.present? && self.parent.parent.blank?
+    return 3 if self.parent.present? && self.parent.parent.present? && self.parent.parent.parent.blank?
+    return 0
+  end 
 end
