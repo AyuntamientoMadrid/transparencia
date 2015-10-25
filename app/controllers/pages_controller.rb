72d00bc957f5c4abaeea6f164d6ecff6d1ee8752
Carlos Guzmán <carlos@guzmanweb.com>
2015-10-25 19:53:35 +0100

    Edit and New pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   before_action :set_selected
+  before_action :set_page, :only => [:edit, :update]
 
   def index
     @pages_section1 = Page.where(parent_id: nil)
@@ -24,18 +25,40 @@ class PagesController < ApplicationController
   end
 
   def edit
+    
   end
 
   def create
+    page = Page.new(page_params)
+    if page.save
+      redirect_to pages_path(selected: @page.parent), notice: "Se ha añadido contenido correctamente"
+    else
+      flash.now[:alert] = "Hubo un error al guardar el contenido, revise el formulario"
+      render :new
+    end
   end
 
   def update
+    if @page.update(page_params)
+      redirect_to pages_path(selected: @page.parent), notice: "Se modificó el contenido correctamente."
+    else
+      flash.now[:alert] = "Hubo un error a guardar el contenido. Revise el formulario."
+      render :edit
+    end
   end
 
   private
 
-  def set_selected
-    @selected = Page.find(params[:selected]) if params[:selected].present?
-  end
+    def set_selected
+      @selected = Page.find(params[:selected]) if params[:selected].present?
+    end
+
+    def set_page
+      @page = Page.find(params[:id])
+    end
+
+    def page_params
+      params.require(:page).permit(:title, :subtitle, :content, :side_content, :link, :parent_id)
+    end
 
 end
