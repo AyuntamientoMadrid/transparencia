7396c96129bfec3b51a61fa5cd5fae72b9d79907
Carlos Guzmán <carlos@guzmanweb.com>
2015-10-25 16:39:17 +0100

    Add pages controller and views

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,21 @@
+class PagesController < ApplicationController
+  
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+end
