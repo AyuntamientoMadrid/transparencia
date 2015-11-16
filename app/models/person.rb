716f59406a91aacd14427e14cb71e82e14b0999d
kikito <kikito@gmail.com>
2015-11-16 15:23:56 +0100

    Adds most_recent_assets_declaration to Person

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,6 +2,7 @@ class Person < ActiveRecord::Base
 
   belongs_to :party
   has_many :assets_declarations
+  has_one  :most_recent_assets_declaration, ->{ order('start_date DESC') }, class_name: "AssetsDeclaration"
 
   validates :name,   presence: true
   validates :email,  presence: true
