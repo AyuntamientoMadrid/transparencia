981f6b4b93cc46815c8c2d94e6c9ca6b1fd9e1a8
kikito <kikito@gmail.com>
2015-11-26 18:29:39 +0100

    Adds internal code to people table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151126154044) do
+ActiveRecord::Schema.define(version: 20151126171405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 20151126154044) do
     t.text    "biography"
     t.text    "functions"
     t.string  "diary"
+    t.integer "internal_code"
   end
 
   create_table "visits", id: :uuid, default: nil, force: :cascade do |t|
