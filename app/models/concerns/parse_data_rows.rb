0b9af93a04171d011fc449ca7942f7a2702d4cda
kikito <kikito@gmail.com>
2015-11-26 15:34:11 +0100

    Makes ParseDataRows immune to nils

--- app/models/concerns/parse_data_rows.rb
+++ app/models/concerns/parse_data_rows.rb
@@ -3,6 +3,7 @@ require 'ostruct'
 module ParseDataRows
 
   def parse_data_rows(data_hash, collection_name)
+    data_hash ||= {}
     (data_hash[collection_name.to_s] || []).collect{ |row| parse_data_row(row) }
   end
 
