cf20478445d0b549dac24917a899ce781526b11b
kikito <kikito@gmail.com>
2015-11-26 15:58:03 +0100

    Remove salary from person in the db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123162423) do
+ActiveRecord::Schema.define(version: 20151126145342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,7 +101,6 @@ ActiveRecord::Schema.define(version: 20151123162423) do
     t.string  "email"
     t.string  "gender"
     t.string  "role"
-    t.float   "salary"
     t.text    "biography"
     t.text    "functions"
   end
