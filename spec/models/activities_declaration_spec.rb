1ceda558dd1a1e769b509546237edd85b2a6be44
Juanjo Bazán <jjbazan@gmail.com>
2015-11-28 00:06:11 +0100

    adds private_activities management to ActivitiesDeclarations

--- spec/models/activities_declaration_spec.rb
+++ spec/models/activities_declaration_spec.rb
@@ -3,11 +3,19 @@ require 'rails_helper'
 describe ActivitiesDeclaration do
 
   describe 'Data accesors' do
-    it 'public_activities should retrieve public_activities data from hstore' do
+    it 'should retrieve public_activities data from hstore' do
       declaration = create(:activities_declaration, data: { public_activities: [{'entity' => 'X', 'position' => 'Y' }]})
       activity = declaration.public_activities.first
       expect(activity.entity).to eq('X')
       expect(activity.position).to eq('Y')
+    end
+
+    it 'should retrieve private_activities data from hstore' do
+      declaration = create(:activities_declaration, data: { private_activities: [{'kind' => 'A', 'entity' => 'B', 'position' => 'C' }]})
+      activity = declaration.private_activities.first
+      expect(activity.kind).to eq('A')
+      expect(activity.entity).to eq('B')
+      expect(activity.position).to eq('C')
     end
   end
 
@@ -21,6 +29,19 @@ describe ActivitiesDeclaration do
       expect(public_activity.position).to eq 'Position'
       expect(public_activity.start_date).to eq Date.parse('1/1/2015')
       expect(public_activity.end_date).to eq Date.parse('1/1/2016')
+    end
+
+    it 'adds private activities to data' do
+      declaration = create(:activities_declaration)
+      declaration.add_private_activity('Private', 'Very Private', 'Entity', 'Position', '1/1/2016', '1/1/2017')
+
+      private_activity = declaration.private_activities.last
+      expect(private_activity.kind).to eq 'Private'
+      expect(private_activity.description).to eq 'Very Private'
+      expect(private_activity.entity).to eq 'Entity'
+      expect(private_activity.position).to eq 'Position'
+      expect(private_activity.start_date).to eq Date.parse('1/1/2016')
+      expect(private_activity.end_date).to eq Date.parse('1/1/2017')
     end
   end
 
