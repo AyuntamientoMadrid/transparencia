98bbb7bbb0fdb425bf85ca40fdaafa205896f184
rgarcia <voodoorai2000@gmail.com>
2015-11-27 10:46:54 +0100

    adds http basic auth to entire site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
+  before_action :authenticate_http_basic, if: :http_basic_auth_site?
 
   add_flash_types :contact_notice, :contact_alert
 
@@ -14,4 +15,16 @@ class ApplicationController < ActionController::Base
       session[:locale] ||= I18n.default_locale
       I18n.locale = session[:locale]
     end
+
+    def authenticate_http_basic
+      authenticate_or_request_with_http_basic do |username, password|
+        username == Rails.application.secrets.http_basic_username &&
+        password == Rails.application.secrets.http_basic_password
+      end
+    end
+
+    def http_basic_auth_site?
+      Rails.application.secrets.http_basic_auth
+    end
+
 end
