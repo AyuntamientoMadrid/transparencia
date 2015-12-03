854dc3c169fd4377cd696dd058c19e7c526d492e
Juanjo Bazán <jjbazan@gmail.com>
2015-12-03 14:57:29 +0100

    paginates subventions

--- app/controllers/subventions_controller.rb
+++ app/controllers/subventions_controller.rb
@@ -1,7 +1,7 @@
 class SubventionsController < ApplicationController
 
   def index
-    @subventions = Subvention.all.order(year: :desc)
+    @subventions = Subvention.all.order(year: :desc).page(params[:page])
   end
 
 end
