8bc75650327a555eac14408525e1c8745eafcf68
Juanjo Bazán <jjbazan@gmail.com>
2015-12-03 12:28:43 +0100

    shows subventions amount with proper format

--- spec/features/subventions_spec.rb
+++ spec/features/subventions_spec.rb
@@ -4,7 +4,7 @@ feature 'Subventions' do
 
   scenario 'Index' do
     subventions = [create(:subvention),
-                   create(:subvention, project: "Green", year: "2017", location: "Malmö")]
+                   create(:subvention, project: "Green", year: "2017", location: "Malmö", amount_euro_cents: 987654321)]
     visit subventions_path
 
     subventions.each do |subvention|
@@ -12,7 +12,7 @@ feature 'Subventions' do
       expect(page).to have_content(subvention.project)
       expect(page).to have_content(subvention.year)
       expect(page).to have_content(subvention.location)
-      expect(page).to have_content(subvention.amount_euro_cents)
+      expect(page).to have_content(number_to_currency(subvention.amount_euro_cents/100.0))
     end
   end
 
