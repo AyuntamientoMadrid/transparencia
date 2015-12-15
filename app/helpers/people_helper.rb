6091683bb14e97ede8d796047d3523b7d1b7bd56
kikito <kikito@gmail.com>
2015-12-15 16:11:40 +0100

    refactor sorting by name initial

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -3,4 +3,8 @@ module PeopleHelper
   def profile_picture(person)
     image_tag "people/#{person.friendly_id}.jpg", alt: person.name
   end
+
+  def name_initial(person)
+    ActiveSupport::Inflector.transliterate(person.name[0]).upcase
+  end
 end
