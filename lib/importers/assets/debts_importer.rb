7578ddc3a702372d34952b00fc837574f5093683
kikito <kikito@gmail.com>
2016-09-13 17:39:35 +0200

    do not remove short comments when importing debts

--- lib/importers/assets/debts_importer.rb
+++ lib/importers/assets/debts_importer.rb
@@ -11,7 +11,6 @@ module Importers
           kind           = row[:clase]
           amount         = row[:importe_actual_en_euros]
           comments       = row[:observaciones]
-          comments = nil if comments.is_a?(String) && comments.length <= 3
 
           puts "#{@year} - Importing debt for #{person.name} (#{kind}, #{amount}, #{comments})"
           declaration.add_debt(kind, amount, comments)
