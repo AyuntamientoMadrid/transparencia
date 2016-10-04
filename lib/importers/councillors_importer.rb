dca92232d5a7bf421836ef0874c3303760070843
kikito <kikito@gmail.com>
2016-10-04 17:50:15 +0200

    Imports councillor order and leaving date in importer

--- lib/importers/councillors_importer.rb
+++ lib/importers/councillors_importer.rb
@@ -8,10 +8,12 @@ module Importers
         puts "Importing councillor: #{councillor.name}"
         councillor.job_level = 'councillor'
         councillor.councillor_code = row[:councillor_code]
+        councillor.councillor_order = row[:councillor_order]
         councillor.personal_code = row[:personal_code]
         councillor.party = Party.find_by!(long_name: row[:party_long_name])
         councillor.email = "tmp@madrid.es"
         councillor.role = row[:post]
+        councillor.leaving_date = Date.iso8601(row[:leaving_date]) if row[:leaving_date].present?
         councillor.previous_calendar_until = row[:previous_calendar_until]
         councillor.previous_calendar_url = row[:previous_calendar_url]
         councillor.admin_first_name = transliterate(row[:first_name]).upcase
