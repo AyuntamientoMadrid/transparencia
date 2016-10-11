3b6254d9b12bf04a7965e53a9d125dcb245cbd7c
kikito <kikito@gmail.com>
2016-10-11 13:59:31 +0200

    Removes personal_code and renames councillor_code to num_trans in councillors.csv

--- lib/importers/councillors_importer.rb
+++ lib/importers/councillors_importer.rb
@@ -7,9 +7,8 @@ module Importers
         councillor = Person.find_or_initialize_by(first_name: row[:first_name], last_name: row[:last_name])
         puts "Importing councillor: #{councillor.name}"
         councillor.job_level = 'councillor'
-        councillor.councillor_code = row[:councillor_code]
+        councillor.councillor_code = row[:num_trans]
         councillor.councillor_order = row[:councillor_order]
-        councillor.personal_code = row[:personal_code]
         councillor.party = Party.find_by!(long_name: row[:party_long_name])
         councillor.email = "tmp@madrid.es"
         councillor.role = row[:post]
