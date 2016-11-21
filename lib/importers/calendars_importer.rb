48414735a353fcf486d2197ea2450bfbfeb9130c
kikito <kikito@gmail.com>
2016-11-21 13:56:12 +0100

    adds new calendar file

--- lib/importers/calendars_importer.rb
+++ lib/importers/calendars_importer.rb
@@ -6,9 +6,14 @@ module Importers
         admin_first_name = transliterate(admin_name.last)
         admin_last_name = transliterate(admin_name.first) # not an error, the csv has the names switched
 
-        person = Person.where("admin_first_name = ? AND admin_last_name = ?", admin_first_name, admin_last_name).first!
-        puts "Importing calendar for #{person.name}"
-        person.update(calendar_url: row[:url_publica])
+        person = Person.where("admin_first_name = ? AND admin_last_name = ?", admin_first_name, admin_last_name).first
+        if person
+          puts "Importing calendar for #{person.name}"
+          person.update(calendar_url: row[:url_publica])
+        else
+          puts "-------------------------"
+          puts "PERSON MISSING FOR CALENDAR: #{admin_first_name} #{admin_last_name}"
+        end
       end
     end
   end
