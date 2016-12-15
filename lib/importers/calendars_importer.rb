20ddb20d32141759c71a5221cc26767ad8dba397
kikito <kikito@gmail.com>
2016-12-15 12:41:13 +0100

    makes calendar importer support names without spaces

--- lib/importers/calendars_importer.rb
+++ lib/importers/calendars_importer.rb
@@ -2,9 +2,9 @@ module Importers
   class CalendarsImporter < BaseImporter
     def import!
       each_row do |row|
-        admin_name = row[:titular].split(', ')
-        admin_first_name = transliterate(admin_name.last)
-        admin_last_name = transliterate(admin_name.first) # not an error, the csv has the names switched
+        admin_name = row[:titular].split(',')
+        admin_first_name = transliterate(admin_name.last.strip)
+        admin_last_name = transliterate(admin_name.first.strip) # not an error, the csv has the names switched
 
         person = Person.where("admin_first_name = ? AND admin_last_name = ?", admin_first_name, admin_last_name).first
         if person
