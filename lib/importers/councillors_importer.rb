fe397f6d20bab5d607308e122df1d5ff399d5df5
kikito <kikito@gmail.com>
2017-03-27 17:52:02 +0200

    Adds new councillor info

--- lib/importers/councillors_importer.rb
+++ lib/importers/councillors_importer.rb
@@ -7,8 +7,8 @@ module Importers
         councillor = Person.find_or_initialize_by(first_name: row[:first_name], last_name: row[:last_name])
         puts "Importing councillor: #{councillor.name}"
         councillor.job_level = 'councillor'
-        councillor.councillor_code = row[:num_trans]
-        councillor.personal_code = row[:num_trans]
+        councillor.councillor_code = row[:identificador]
+        councillor.personal_code = row[:identificador]
         councillor.councillor_order = row[:councillor_order]
         councillor.party = Party.find_by!(long_name: row[:party_long_name])
         councillor.email = "tmp@madrid.es"
