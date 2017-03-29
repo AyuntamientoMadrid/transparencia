de725807941f8a7399b80b0bab3bbad662c503ef
kikito <kikito@gmail.com>
2017-03-29 13:28:14 +0200

    Adds first version of profile importer (needs a spec)

--- app/controllers/admin/profile_uploads_controller.rb
+++ app/controllers/admin/profile_uploads_controller.rb
@@ -1,15 +1,39 @@
+require 'stringio'
+require 'logger'
+require 'excel_importers/profile'
+
 class Admin::ProfileUploadsController < Admin::BaseController
-
   def new
-
   end
 
   def create
-    render text: params.inspect
+    file =  profile_upload_params[:file].tempfile
+
+    @log = StringIO.open do |strio|
+      logger = Logger.new(strio)
+      logger.formatter = proc do |severity, datetime, progname, msg|
+        "#{severity}: #{msg}\n"
+      end
+
+      importer = ExcelImporters::Profile.new file,
+                                             headers_row: 2,
+                                             logger: logger
+      if importer.safe_import!
+        flash[:notice] = t('admin.profile_uploads.create.no_errors')
+      else
+        flash[:error] = t('admin.profile_uploads.create.errors')
+      end
+
+      logger.close
+      strio.string
+    end
+
+    render :show
   end
 
-  def show
+  private
 
-  end
-
+    def profile_upload_params
+      params.require(:profile_upload).permit(:file)
+    end
 end
