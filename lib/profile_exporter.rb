d35bc31d022ee968dccde2045a29a7e06c7a5c14
kikito <kikito@gmail.com>
2017-05-19 19:30:42 +0200

    starts working on profile_exporter

---
+++ lib/profile_exporter.rb
@@ -0,0 +1,130 @@
+class ProfileExporter
+  FIELDS = %w{
+    profiled_at
+    first_name
+    last_name
+    role
+    unit
+    twitter
+    facebook
+
+    first_study_description
+    first_study_entity
+    first_study_start_year
+    first_study_end_year
+    second_study_description
+    second_study_entity
+    second_study_start_year
+    second_study_end_year
+    third_study_description
+    third_study_entity
+    third_study_start_year
+    third_study_end_year
+    fourth_study_description
+    fourth_study_entity
+    fourth_study_start_year
+    fourth_study_end_year
+    studies_comment
+
+    first_course_description
+    first_course_entity
+    first_course_start_year
+    first_course_end_year
+    second_course_description
+    second_course_entity
+    second_course_start_year
+    second_course_end_year
+    third_course_description
+    third_course_entity
+    third_course_start_year
+    third_course_end_year
+    fourth_course_description
+    fourth_course_entity
+    fourth_course_start_year
+    fourth_course_end_year
+    courses_comment
+
+    language_english_level
+    language_french_level
+    language_german_level
+    language_italian_level
+    language_other_name
+    language_other_level
+
+    public_jobs_body
+    public_jobs_start_year
+
+    first_public_job_description
+    first_public_job_entity
+    first_public_job_start_year
+    first_public_job_end_year
+    second_public_job_description
+    second_public_job_entity
+    second_public_job_start_year
+    second_public_job_end_year
+    third_public_job_description
+    third_public_job_entity
+    third_public_job_start_year
+    third_public_job_end_year
+    fourth_public_job_description
+    fourth_public_job_entity
+    fourth_public_job_start_year
+    fourth_public_job_end_year
+
+    public_jobs_level
+
+    first_private_job_description
+    first_private_job_entity
+    first_private_job_start_year
+    first_private_job_end_year
+    second_private_job_description
+    second_private_job_entity
+    second_private_job_start_year
+    second_private_job_end_year
+    third_private_job_description
+    third_private_job_entity
+    third_private_job_start_year
+    third_private_job_end_year
+    fourth_private_job_description
+    fourth_private_job_entity
+    fourth_private_job_start_year
+    fourth_private_job_end_year
+
+    career_comment
+
+    first_political_post_description
+    first_political_post_entity
+    first_political_post_start_year
+    first_political_post_end_year
+    second_political_post_description
+    second_political_post_entity
+    second_political_post_start_year
+    second_political_post_end_year
+    third_political_post_description
+    third_political_post_entity
+    third_political_post_start_year
+    third_political_post_end_year
+    fourth_political_post_description
+    fourth_political_post_entity
+    fourth_political_post_start_year
+    fourth_political_post_end_year
+
+    political_posts_comment
+
+    publications
+
+    teaching_activity
+
+    special_mentions
+
+    other
+  }
+
+  def headers
+    FIELDS.map { |f| I18n.t("profile_exporter.#{f}") }
+  end
+
+  def person_to_row(person)
+    FIELDS.map { |f| person.send(f) }
+  end
+end
---
+++ spec/profile_exporter_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+require 'profile_exporter'
+
+describe ProfileExporter do
+  let(:exporter) { ProfileExporter.new }
+
+  describe '#headers' do
+    it "generates localized headers" do
+      expect(exporter.headers.first).to eq('Profiled At')
+      expect(exporter.headers.last).to eq('Others')
+    end
+  end
+
+  describe '#person_to_row' do
+    it "generates a row of info based on a person" do
+      p = create(:person, first_name: 'James T.', last_name: 'Kirk')
+      p.add_study('Officer Training Program', 'Starfleet Academy', 2252, 2257)
+      p.add_course('Ancient Literature', "John Gill's class", 2253, 2253)
+      p.add_language('Inglés', 'Legendary')
+      p.add_language('Klingon', 'jIyaj')
+      p.add_public_job('Starfleet captain', 'Starfleet', 2265, 2270)
+      p.add_private_job('Gorn Tamer', 'Metron Arena', 2266, 2266)
+      p.publications = "Captain's Log"
+
+      row = exporter.person_to_row(p)
+
+      expect(row).to include('Officer Training Program', 'Starfleet Academy', 2252, 2257)
+      expect(row).to include('Ancient Literature', "John Gill's class", 2253, 2253)
+      expect(row).to include('Legendary')
+      expect(row).to include('Klingon', 'jIyaj')
+      expect(row).to include('Starfleet captain', 'Starfleet', 2265, 2270)
+      expect(row).to include('Gorn Tamer', 'Metron Arena', 2266, 2266)
+      expect(row).to include("Captain's Log")
+    end
+  end
+
+end
