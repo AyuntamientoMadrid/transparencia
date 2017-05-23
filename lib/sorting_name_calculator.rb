4d15d5f552ae900eea1639280ec908ec4c266a46
kikito <kikito@gmail.com>
2017-05-23 17:27:03 +0200

    stops using stop worlds on names

--- lib/sorting_name_calculator.rb
+++ lib/sorting_name_calculator.rb
@@ -1,12 +1,10 @@
 module SortingNameCalculator
 
-  STOP_WORDS = %{el, la, las, de, del, los, da, de, do, dos}
-
   def self.calculate(first_name, last_name)
      last_name = transliterate(last_name)
                   .downcase # need this so that spaces don't mess up with sort, never do uppercase here
+                  .gsub('-', '')
                   .split(/\s+/)
-                  .reject{|x| x.in? STOP_WORDS}
                   .join(' ')
 
      first_name = transliterate(first_name).downcase
