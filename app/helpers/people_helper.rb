e6c49154250800062d3bb1a7fcf3cd240db3fd08
Raúl Fuentes <raul.fuentes@m2c.es>
2018-05-03 12:12:20 +0200

    Add the new portrait to the public views
    removed the old profile_picture method.

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,9 +1,5 @@
 module PeopleHelper
 
-  def profile_picture(person)
-    image_tag "people/#{person.friendly_id}.jpg", alt: person.name
-  end
-
   def people_job_level_options_for_select(current_job_level)
     array = [['','']] + Person.job_levels.map{|j| [I18n.t("people.job_levels.#{j}"), j]}
     options_for_select(array, current_job_level)
