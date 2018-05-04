314ed41792ff5a4e56de59d809a4b5f29db56e2a
Raúl Fuentes <raul.fuentes@m2c.es>
2018-05-04 09:57:36 +0200

    Move hide and unhide forms from people to admin/people
    changed the admin/people index now from here we can view links for edit,
    delete, create and hide people.
    modify the left menu with new options.
    add a new list which shows the hidded people and allow to unhide them.

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,9 +1,24 @@
 module PeopleHelper
 
   def people_job_level_options_for_select(current_job_level)
-    array = [['','']] + Person.job_levels.map{|j| [I18n.t("people.job_levels.#{j}"), j]}
+    array = Person.job_levels.map{|j| [I18n.t("people.job_levels.#{j}"), j]}
     options_for_select(array, current_job_level)
   end
 
+  def breadcrumb_person_title_link(person)
+    if person.councillor?
+      link_to t("people.councillors.title"), councillors_people_path
+    elsif person.director?
+      link_to t("people.directors.title"), directors_people_path
+    elsif person.temporary_worker?
+      link_to t("people.temporary_workers.title"), temporary_workers_people_path
+    elsif person.public_worker?
+      link_to t("people.public_workers.title"), public_workers_people_path
+    elsif person.spokesperson?
+      link_to t("people.spokespeople.title"), spokespeople_people_path
+    elsif person.labour?
+      link_to t("people.labours.title"), labours_people_path
+    end
+  end
 
 end
