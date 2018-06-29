ee70afe1daf875f631f3a1c350ab59573d695ecc
Raúl Fuentes <raul.fuentes@m2c.es>
2018-06-29 15:25:57 +0200

    Add helper for the portrait images

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,5 +1,9 @@
 module PeopleHelper
 
+  def profile_picture(person, size = nil)
+    image_tag person.portrait.url(size || :medium), alt: person.name
+  end
+
   def people_job_level_options_for_select(current_job_level)
     array = Person.job_levels.map{|j| [I18n.t("people.job_levels.#{j}"), j]}
     options_for_select(array, current_job_level)
