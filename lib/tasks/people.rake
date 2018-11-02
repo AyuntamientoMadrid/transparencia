a4d7fd42aed9fa1b0c9902edfda97587b599af41
Javi Martín <javim@elretirao.net>
2018-11-02 16:57:56 +0100

    Add task to migrate existing portraits

--- lib/tasks/people.rake
+++ lib/tasks/people.rake
@@ -5,4 +5,17 @@ namespace :people do
       p.save
     end
   end
+
+  desc "migrates existing portraits to paperclip objects"
+  task migrate_portraits: :environment do
+    Person.find_each do |person|
+      image_path = "#{Rails.root}/app/assets/images/people/#{person.friendly_id}.jpg"
+
+      if File.exists?(image_path)
+        person.update(portrait: File.new(image_path))
+      else
+        puts "No existing image available for #{person.name}; skipping"
+      end
+    end
+  end
 end
---
+++ spec/lib/tasks/people_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+require "rake"
+
+describe "People tasks" do
+  before do
+    Rake.application.rake_require "tasks/people"
+    Rake::Task.define_task(:environment)
+  end
+
+  describe "#migrate_portraits" do
+    let :run_rake_task do
+      Rake::Task["people:migrate_portraits"].reenable
+      Rake.application.invoke_task "people:migrate_portraits"
+    end
+
+    context "Existing image" do
+      let!(:person) { create(:person, first_name: "Pablo", last_name: "Soto Bravo") }
+
+      it "attaches the image" do
+        run_rake_task
+        person.reload
+
+        expect(person.portrait).to be_present
+      end
+    end
+
+    context "Non-existing image" do
+      let!(:person) { create(:person, first_name: "Red", last_name: "Richards") }
+
+      it "doesn't attach the image" do
+        run_rake_task
+        person.reload
+
+        expect(person.portrait).not_to be_present
+      end
+    end
+  end
+end
+
